b5bc0b88ed7de0746cf3bc7ea249d94a47d20782
Martin Mahner <martin@mahner.org>
2015-10-15 16:09:45 +0200

    Treat Organization as read-only

--- hub/apps/metadata/admin.py
+++ hub/apps/metadata/admin.py
@@ -5,8 +5,18 @@ from . import models
 class SustainabilityTopicAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
+
 class OrganizationAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        """Treat all fields as read-only"""
+        return [i.name for i in obj._meta.fields]
+
     def has_add_permission(self, request):
+        """Nobody is allowed to add records."""
+        return False
+
+    def has_delete_permission(self, request, obj):
+        """Nobody is allowed to add records."""
         return False
 
 admin.site.register(models.SustainabilityTopic, SustainabilityTopicAdmin)
