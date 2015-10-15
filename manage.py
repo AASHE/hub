fa1b2a6ec3b9c4daac05549f7db631921fce74f4
Benjamin Stookey <ben.stookey@gmail.com>
2015-10-15 13:21:20 -0400

    Update manage.py to use same config method as wsgi.py;

--- manage.py
+++ manage.py
@@ -5,6 +5,12 @@ import sys
 # Other than the wsgi.py file, this manage.py file always uses the `local`
 # settings, since it is only ran on dev machines.
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hub.settings.local")
+    # Use the local settings file if one is present, otherwise the server.
+    try:
+        import hub.settings.local
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hub.settings.local")
+    except ImportError:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hub.settings.server")
+
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
