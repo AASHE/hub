749b09181fc5e88e245ed3b038453f5ab9ca2824
Martin Mahner <martin@mahner.org>
2015-11-10 09:39:50 +0100

    Better form field rendering.
    It’s now based on the widget rather a funky string matching.

--- hub/apps/browse/templatetags/browse_tags.py
+++ hub/apps/browse/templatetags/browse_tags.py
@@ -2,8 +2,10 @@ from __future__ import unicode_literals
 
 from logging import getLogger
 
+from django.forms import widgets
 from django.template import Library
 from django.utils.safestring import mark_safe
+from django.template.loader import render_to_string
 
 logger = getLogger(__name__)
 register = Library()
@@ -24,3 +26,53 @@ def permission_flag(obj, user):
     if flag == 'member-required':
         return mark_safe(label.format(label='Membership Required'))
     return ''
+
+
+# <widget>: <template name to render>
+FIELD_MAP = {
+    widgets.HiddenInput: 'hidden',
+    widgets.CheckboxInput: 'checkbox',
+    widgets.CheckboxSelectMultiple: 'checkbox_multiple',
+    widgets.Select: 'select',
+    widgets.SelectMultiple: 'select_multiple',
+    widgets.RadioSelect: 'checkbox_multiple',
+    widgets.Textarea: 'textarea',
+}
+
+
+@register.simple_tag
+def render_form(form, field=None, type='input'):
+    """
+    Renders a Django form, field by field, while rendering each field with a
+    special template according to it's widget.
+
+    We need to do that since the HTML layout of a Bootstrap field differs
+    heavily per type.
+    """
+    response = ''
+
+    # Fetch a list of all rendered fields, as well as the base fields of the
+    # form. The latter gives us access to the widget and other attributes we
+    # need later.
+    if field:  # Just this specific field
+        field_list = [form.fields.get(field)]
+        rendered_field_list = [f for f in form.__iter__() if f.name == field]
+    else:  # Or all fields
+        field_list = form.fields.values()
+        rendered_field_list = list(form.__iter__())
+
+    # Attach each field to the  rendered field
+    i = 0
+    for f in rendered_field_list:
+        f._field = field_list[i]
+        i += 1
+
+    # Iterate over all fields and render each one with the matching template
+    for f in rendered_field_list:
+        widget = f._field.widget.__class__
+        field_type = FIELD_MAP.get(widget, 'input')
+        template_name = 'forms/{}.html'.format(field_type)
+        response += render_to_string(template_name, {'field': f})
+        logger.debug('{}: {}'.format(widget, field_type))
+
+    return mark_safe(response)
