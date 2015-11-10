5d60ff9a6d78c80e6985160e2f175419f475899a
Martin Mahner <martin@mahner.org>
2015-11-10 13:04:21 +0100

    Updated affirmation text.

--- hub/apps/content/types/strings.py
+++ hub/apps/content/types/strings.py
@@ -1,9 +1,9 @@
-AFFIRMATION = """By checking this box, you are granting AASHE an irrevocable,
-royalty-free, non-exclusive and perpetual license to use the submitted documents
-in publications, newsletters, resources or promotional materials, and you are
-hereby representing and warranting that you own all the rights to the submitted
-image, or have obtained all necessary licenses and/or permissions to use the
-submitted image, and that AASHE's use of such image will not infringe the rights
-of any third party, including but not limited to intellectual property rights,
-or any other rights protected by law (such as the right to privacy or right of
-publicity).""".replace('\n', ' ')
+AFFIRMATION = """This should work: By checking this box, you are granting AASHE
+an irrevocable, royalty-free, non-exclusive and perpetual license to use the
+submitted file(s) in publications, newsletters, resources or promotional
+materials, and you are hereby representing and warranting that you own all the
+rights to the submitted file(s), or have obtained all necessary licenses and/or
+permissions to use the submitted file(s), and that AASHE's use of such file(s)
+will not infringe the rights of any third party, including but not limited to
+intellectual property rights, or any other rights protected by law (such as the
+right to privacy or right of publicity).""".replace('\n', ' ')
