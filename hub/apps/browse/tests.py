c817b7c08f056563760d8a34c32370ce4e266a64
Martin Mahner <martin@mahner.org>
2015-11-12 21:47:17 +0100

    Unittests to cover the different browse permission behaviors.

--- hub/apps/browse/tests.py
+++ hub/apps/browse/tests.py
@@ -1,7 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
+
 from ..content.types.academic import AcademicProgram
+from ..content.types.videos import Video
 from ..content.models import ContentType
+from ..metadata.models import SustainabilityTopic
+
+from .views import PUBLIC_CONTENT_TYPES
 
 User = get_user_model()
 
@@ -88,4 +94,105 @@ class ContentTypePermissionTestCase(WithUserSuperuserTestCase):
         # Logged in and member/superuser
         self.client.login(**self.superuser_cred)
         response = self.client.get(self.ct.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+
+
+class BrowsePermissionTestCase(WithUserSuperuserTestCase):
+    """
+    Test cases around the different permissions for the browse view
+    (search results).
+
+    - The homepage is open for all audiences
+    - Search list browse results need auth
+    - Topic list browse results need auth
+    - Content Type browse results need auth
+      - except certain 'PUBLIC_CONTENT_TYPES' content types
+        which results are  open to unauthed users as well
+    """
+    def setUp(self):
+        # Setup some content
+        self.sus = SustainabilityTopic.objects.create(name='Research', slug='research')
+
+        # URLs
+        self.url_home = reverse('home')
+        self.url_search = '{}?search=keyword'.format(reverse('browse:browse'))
+        self.url_topic = reverse('browse:browse', kwargs={'topic': self.sus.slug})
+        self.url_ct = reverse('browse:browse', kwargs={'ct': 'video'})
+
+        return super(BrowsePermissionTestCase, self).setUp()
+
+    def test_homepage_is_visible_to_all(self):
+        """
+        Homepage is visible to all users, no matter if authed or not.
+        """
+        self.client.logout()
+        response = self.client.get(self.url_home)
+        self.assertEqual(response.status_code, 200)
+
+        self.client.login(**self.user_cred)
+        response = self.client.get(self.url_home)
+        self.assertEqual(response.status_code, 200)
+
+        self.client.login(**self.superuser_cred)
+        response = self.client.get(self.url_home)
+        self.assertEqual(response.status_code, 200)
+
+    def test_topic_view_is_auth_only(self):
+        """
+        Unauthed members can't see the topic view.
+        """
+        self.client.logout()
+        response = self.client.get(self.url_topic)
+        self.assertEqual(response.status_code, 403)
+
+        self.client.login(**self.user_cred)
+        response = self.client.get(self.url_topic)
+        self.assertEqual(response.status_code, 200)
+
+        self.client.login(**self.superuser_cred)
+        response = self.client.get(self.url_topic)
+        self.assertEqual(response.status_code, 200)
+
+    def test_content_type_view_is_auth_only(self):
+        """
+        Unauthed members can't see the topic view.
+        """
+        self.client.logout()
+        response = self.client.get(self.url_ct)
+        self.assertEqual(response.status_code, 403)
+
+        self.client.login(**self.user_cred)
+        response = self.client.get(self.url_ct)
+        self.assertEqual(response.status_code, 200)
+
+        self.client.login(**self.superuser_cred)
+        response = self.client.get(self.url_ct)
+        self.assertEqual(response.status_code, 200)
+
+    def test_open_ct_is_visible_to_anybody(self):
+        """
+        Certain, OPEN content types are visible to anybody.
+        """
+        if not PUBLIC_CONTENT_TYPES:
+            return
+        open_url = reverse('browse:browse', kwargs={
+            'ct': PUBLIC_CONTENT_TYPES[0]})
+        self.client.logout()
+        response = self.client.get(open_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_search_view_is_auth_only(self):
+        """
+        Unauthed members can't see the topic view.
+        """
+        self.client.logout()
+        response = self.client.get(self.url_search)
+        self.assertEqual(response.status_code, 403)
+
+        self.client.login(**self.user_cred)
+        response = self.client.get(self.url_search)
+        self.assertEqual(response.status_code, 200)
+
+        self.client.login(**self.superuser_cred)
+        response = self.client.get(self.url_search)
         self.assertEqual(response.status_code, 200)
