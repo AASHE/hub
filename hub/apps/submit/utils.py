20863d53f49c5ff9fc9978f535e339dee175ae58
rerb <bob.erb@aashe.org>
2015-12-01 11:04:17 -0500

    Don't fail to send email silently
    Log an error instead.

--- hub/apps/submit/utils.py
+++ hub/apps/submit/utils.py
@@ -1,5 +1,10 @@
+import logging
+
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+
+
+logger = logging.getLogger(__name__)
 
 NEW_SUBMITTED_RESOURCE_EMAIL_TEMPLATE = 'new_submitted_resource_email.txt'
 
@@ -8,8 +13,10 @@ def send_new_submitted_resource_email(resource, request):
     content = render_to_string(NEW_SUBMITTED_RESOURCE_EMAIL_TEMPLATE,
                                {'resource': resource,
                                 'request': request})
-    send_mail(subject='New Submitted Resource for Review',
-              message=content,
-              from_email='resources@aashe.org',
-              recipient_list=['bob@aashe.org'],
-              fail_silently=True)
+    try:
+        send_mail(subject='New Submitted Resource for Review',
+                  message=content,
+                  from_email='resources@aashe.org',
+                  recipient_list=['resources@aashe.org'])
+    except:
+        logger.exception('Failed to send mail for new submitted resource.')
