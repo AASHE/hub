1aa402f810460bac6c24bb4ce2d8f2dc2795cec8
Benjamin Stookey <ben.stookey@gmail.com>
2016-03-15 00:40:07 -0600

    Selectize.js in the admin; fixes #56 and fixes #88;

--- hub/apps/content/admin.py
+++ hub/apps/content/admin.py
@@ -30,8 +30,21 @@ class ImageInline(admin.TabularInline):
     extra = 0
 
 
-class BaseContentTypeAdmin(admin.ModelAdmin):
+# Custom model form for the admin
+from django import forms
+from ..browse.forms import LeanSelectMultiple  
+class ContentTypeAdminForm(forms.ModelForm):
+  class Meta:
+    model = ContentType
+    fields = '__all__'
+    widgets = {
+      'organizations': LeanSelectMultiple(),
+    }
 
+
+class BaseContentTypeAdmin(admin.ModelAdmin):
+    form = ContentTypeAdminForm
+    
     def save_model(self, request, obj, form, change):
         status_tracker_changed = obj.status_tracker.changed()  # before save
         obj.save()
@@ -49,7 +62,7 @@ class SpecificContentTypeAdmin(BaseContentTypeAdmin):
     readonly_fields = ('published',)
     inlines = (AuthorInline, WebsiteInline, FileInline, ImageInline)
     exclude = ('content_type',)
-    raw_id_fields = ('organizations', 'submitted_by')
+    raw_id_fields = ('submitted_by',)
 
     def _update_application_index(self):
         # from django.core import management
