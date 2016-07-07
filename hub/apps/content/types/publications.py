98c5a59f7e7b7dd0fd33973f2357e72c67d945c2
Scott Johnson <baronvonvaderham@gmail.com>
2016-07-07 17:05:12 -0400

    Remove Old Field from Publication Model

--- hub/apps/content/types/publications.py
+++ hub/apps/content/types/publications.py
@@ -8,16 +8,6 @@ from ...metadata.models import PublicationMaterialType
 
 
 class Publication(ContentType):
-    TYPE_CHOICES = Choices(
-        ('book', 'Book'),
-        ('book chapter', 'Book Chapter'),
-        ('journal article', 'Journal Article'),
-        ('news', 'News or Magazine Article'),
-        ('blog', 'Blog Article'),
-        ('report', 'Published Report'),
-        ('graduate', 'Graduate Student Research'),
-        ('undergrad', 'Undergraduate Student Research'),
-    )
 
     publisher = models.CharField('Publisher', max_length=200,
         blank=True, null=True, help_text='Enter the name of the publisher, if applicable.')
@@ -25,12 +15,6 @@ class Publication(ContentType):
         max_length=200, blank=True, null=True, help_text='''Enter the name of
         the periodical (e.g., journal, magazine, newspaper), if applicable. For
         book chapers, enter the title of the book.''')
-    _type = models.CharField(max_length=40, choices=TYPE_CHOICES, null=True,
-        verbose_name='Type of Material',
-        help_text='''"Journal Article," "Graduate Student Research" and
-        "Undergraduate Student Research" submissions will be automatically
-        considered for a Campus Sustainability Research Award as part of
-        AASHE's annual awards program.''')
     material_type = models.ForeignKey(PublicationMaterialType, null=True,
         verbose_name='Type of Material',
         help_text='''"Journal Article," "Graduate Student Research" and
