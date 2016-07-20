abcfed1665f26c6afa40ae307d5c2eca930ef858
Benjamin Stookey <ben.stookey@gmail.com>
2016-07-20 10:30:46 -0600

    Added progress bar when copying to new bucket;

--- hub/exports/management/commands/copy_to_new_bucket.py
+++ hub/exports/management/commands/copy_to_new_bucket.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from hub.apps.content.models import File, Image
 
 import boto
+from fish import ProgressFish
 from urlparse import urlparse
 
 
@@ -14,12 +15,26 @@ class Command(BaseCommand):
         src = conn.get_bucket('aashe-hub-dev')
         dst = conn.get_bucket('aashe-hub-production')
 
-        for f in File.objects.filter(item__isnull=False):
+        print "Copying all Files..."
+
+        file_qs = File.objects.filter(item__isnull=False)
+        fish = ProgressFish(total=file_qs.count())
+        count = 0
+        for f in file_qs[:3]:
+            count += 1
+            fish.animate(amount=count)
             key = urlparse(f.item).path[1:]
-            print "copying: %s" % key
             dst.copy_key(key, src.name, key)
 
-        for i in Image.objects.filter(image__isnull=False):
+        print "Copying all Images..."
+
+        image_qs = Image.objects.filter(image__isnull=False)
+        fish2 = ProgressFish(total=image_qs.count())
+        count = 0
+        for i in image_qs[:3]:
+            count += 1
+            fish.animate(amount=count)
             key = urlparse(i.image).path[1:]
-            print "copying: %s" % key
             dst.copy_key(key, src.name, key)
+
+        print
