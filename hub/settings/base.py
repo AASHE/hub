3a2e153ae31de160128ec0171f4ec5a65248745a
Benjamin Stookey <ben.stookey@gmail.com>
2016-08-03 12:29:55 -0600

    switched SITE_DOMAIN to 'testserver'; and some pep8 love;

--- hub/settings/base.py
+++ hub/settings/base.py
@@ -320,10 +320,13 @@ S3DIRECT_DESTINATIONS = {
 # ==============================================================================
 # Link checking
 # ==============================================================================
-LINKCHECK_EXTERNAL_RECHECK_INTERVAL = os.environ.get('LINKCHECK_EXTERNAL_RECHECK_INTERVAL', 10080)
-LINKCHECK_EXTERNAL_REGEX_STRING = os.environ.get('LINKCHECK_EXTERNAL_REGEX_STRING', r'^https?://')
+LINKCHECK_EXTERNAL_RECHECK_INTERVAL = os.environ.get(
+    'LINKCHECK_EXTERNAL_RECHECK_INTERVAL', 10080)
+LINKCHECK_EXTERNAL_REGEX_STRING = os.environ.get(
+    'LINKCHECK_EXTERNAL_REGEX_STRING', r'^https?://')
 LINKCHECK_MEDIA_PREFIX = os.environ.get('LINKCHECK_MEDIA_PREFIX', '/media/')
 LINKCHECK_RESULTS_PER_PAGE = os.environ.get('LINKCHECK_RESULTS_PER_PAGE', 25)
 LINKCHECK_MAX_URL_LENGTH = os.environ.get('LINKCHECK_MAX_URL_LENGTH', 255)
-LINKCHECK_CONNECTION_ATTEMPT_TIMEOUT = os.environ.get('LINKCHECK_CONNECTION_ATTEMPT_TIMEOUT', 10)
-SITE_DOMAIN = os.environ.get('SITE_DOMAIN', 'hub.aashe.org')
+LINKCHECK_CONNECTION_ATTEMPT_TIMEOUT = os.environ.get(
+    'LINKCHECK_CONNECTION_ATTEMPT_TIMEOUT', 10)
+SITE_DOMAIN = os.environ.get('SITE_DOMAIN', 'testserver')
