4ad7c75114b0e5f4b51c955dc76ae636633cf256
rerb <bob.erb@aashe.org>
2016-08-29 18:25:41 -0400

    Upgrade Django, whitenoise, and raven
    Part of whitenoise upgrade is moving configuration from wsgi.py to
    MIDDLEWARE_CLASSES in settings.

--- hub/wsgi.py
+++ hub/wsgi.py
@@ -16,9 +16,6 @@ try:
 except ImportError:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hub.settings.server")
 
-
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
