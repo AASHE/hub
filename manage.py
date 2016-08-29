7428877d26c3b8d0325377fd996440234b77868e
rerb <bob.erb@aashe.org>
2016-08-29 10:41:17 -0400

    Use dev_settings.py if avail and no locals.py is
    dev_settings.py is settings.py with django_extensions and, optionally,
    django-debug-toolbar (dependent on DEBUG_TOOLBAR environment var)
    installed.

--- manage.py
+++ manage.py
@@ -6,11 +6,17 @@ import sys
 # settings, since it is only ran on dev machines.
 if __name__ == "__main__":
     # Use the local settings file if one is present, otherwise the server.
-    try:
+    try:  # Use local settings if available.
         import hub.settings.local
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hub.settings.local")
-    except ImportError:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hub.settings.server")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                              "hub.settings.local")
+    except ImportError:  # Else, use dev_server settings if available.
+        import hub.settings.dev_server  # NOQA
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                              "hub.settings.dev_server")
+    except ImportError:  # Else, use server settings.
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                              "hub.settings.server")
 
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
