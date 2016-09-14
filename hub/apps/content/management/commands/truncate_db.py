5ef4de83aebb4c017cd762099007a42e47b6d3e1
Benjamin Stookey <ben.stookey@gmail.com>
2016-09-14 10:30:24 -0300

    Updates to truncate_db;

--- hub/apps/content/management/commands/truncate_db.py
+++ hub/apps/content/management/commands/truncate_db.py
@@ -1,4 +1,8 @@
-from hub.apps.content.models import CONTENT_TYPES
+from hub.apps.content.models import CONTENT_TYPES, Image, File, Website, Author
+from hub.apps.metadata.models import (
+    AcademicDiscipline, InstitutionalOffice, ProgramType, ConferenceName,
+    PresentationType, CourseMaterialType, OutreachMaterialType,
+    PublicationMaterialType, Organization)
 from django.core.management.base import BaseCommand, CommandError
 
 
@@ -24,13 +28,35 @@ class Command(BaseCommand):
             'This is a DESTRUCTIVE action. Are you sure? [y/N]: ')
 
         if confirm == 'y':
+
+            # start with Organization, as this will cascade heavily
+            print "====================="
+            print "Deleting Organizations"
+            self.delete_objects_of_class(Organization, retain=100)
+            print
+
+            print "====================="
+            print "Deleting Content Types"
             print
             for k, ctKlass in CONTENT_TYPES.items():
-                to_delete = ctKlass.objects.all()[20:]
-                print "Deleting %d of %d %s objects." % (
-                    to_delete.count(), ctKlass.objects.count(), k)
-                for obj in to_delete:
-                    obj.delete()
+                self.delete_objects_of_class(ctKlass)
             print
+
+            print "====================="
+            print "Deleting Linked Objects and Metadata"
+            class_list = [
+                Image, File, Website, Author, AcademicDiscipline,
+                InstitutionalOffice, ProgramType, ConferenceName,
+                PresentationType, CourseMaterialType, OutreachMaterialType,
+                PublicationMaterialType]
+            for klass in class_list:
+                self.delete_objects_of_class(klass)
         else:
             print "Action Cancelled."
+
+    def delete_objects_of_class(self, klass, retain=20):
+        to_delete = klass.objects.all()[retain:]
+        print "Deleting %d of %d %s objects." % (
+            to_delete.count(), klass.objects.count(), klass.__name__)
+        for obj in to_delete:
+            obj.delete()
