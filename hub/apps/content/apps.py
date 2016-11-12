f2a7e91a9902412f8073947c9a62d2c4e3a22451
rerb <bob.erb@aashe.org>
2016-11-12 18:13:21 -0500

    Move content signals from models.py to signals.py

--- hub/apps/content/apps.py
+++ hub/apps/content/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ContentTypesConfig(AppConfig):
     name = 'hub.apps.content'
     verbose_name = 'Content Types'
+
+    def ready(self):
+        import signals  # Register signal handlers.  # noqa
--- hub/apps/content/models.py
+++ hub/apps/content/models.py
@@ -11,8 +11,6 @@ from django.contrib.postgres.search import (SearchVector,
                                             SearchVectorField)
 from django.core.urlresolvers import reverse
 from django.db import models
-from django.db.models.signals import post_delete, post_save
-from django.dispatch import receiver
 from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 from model_utils import Choices, FieldTracker
@@ -430,66 +428,6 @@ class Image(TimeStampedModel):
 
     def get_absolute_url(self):
         return self.ct.get_admin_url()
-
-
-@receiver(post_save, sender=Author, weak=False,
-          dispatch_uid=('hub.apps.content.models.author.post_save'
-                        '.update_authors_search_data'))
-@receiver(post_delete, sender=Author, weak=False,
-          dispatch_uid=('hub.apps.content.models.author.post_delete'
-                        '.update_authors_search_data'))
-def update_authors_search_data(sender, instance, **kwargs):
-    """
-    Update instance.ct.authors_search_data.
-    """
-    # Really only want to do this when instance.name changes, but maybe
-    # that would require premature or impractical optimization -- namely
-    # add a FieldTracker field for ContentType.name.  Or maybe that's a
-    # good idea ... for later.
-    content_type_model = CONTENT_TYPES[instance.ct.content_type]
-    content_type = content_type_model.objects.get(
-        pk=instance.ct.pk)
-    content_type.authors_search_data = " ".join(
-        [author.name for author in content_type.authors.all()
-         if author.name]).strip()
-    content_type.save()
-
-
-@receiver(post_save, sender=File, weak=False,
-          dispatch_uid='hub.apps.content.models.file.post_save')
-@receiver(post_delete, sender=File, weak=False,
-          dispatch_uid='hub.apps.content.models.file.post_delete')
-def update_files_search_data(sender, instance, **kwargs):
-    """
-    Update instance.ct.files_search_data.
-    """
-    content_type_model = CONTENT_TYPES[instance.ct.content_type]
-    content_type = content_type_model.objects.get(
-        pk=instance.ct.pk)
-    content_type.files_search_data = " ".join(
-        [f.label for f in content_type.files.all()
-         if f.label]).strip()
-    content_type.save()
-
-
-@receiver(post_save, sender=Image, weak=False,
-          dispatch_uid='hub.apps.content.models.image.post_save')
-@receiver(post_delete, sender=Image, weak=False,
-          dispatch_uid='hub.apps.content.models.image.post_delete')
-def update_images_search_data(sender, instance, **kwargs):
-    """
-    Update instance.ct.images_search_data.
-    """
-    content_type_model = CONTENT_TYPES[instance.ct.content_type]
-    content_type = content_type_model.objects.get(
-        pk=instance.ct.pk)
-    content_type.images_search_data = " ".join(
-        [image.caption for image in content_type.images.all()
-         if image.caption]).strip()
-    content_type.images_search_data += " " + " ".join(
-        [image.credit for image in content_type.images.all()
-         if image.credit]).strip()
-    content_type.save()
 
 
 def update_all_content_type_search_data():
