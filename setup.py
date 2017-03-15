461894ddfe2833ec98e571b1df6c337a62dfe848
Benjamin W Stookey <ben.stookey@gmail.com>
2017-03-15 17:37:00 -0600

    Updated authors and description;
    Technically this syntax isn't supported:
    
    http://bugs.python.org/issue6992
    
    but since this isn't a package, I'm ok with it.

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup, find_packages
 setup(
     name='hub',
     version='1.0',
-    description="",
-    author="Lincoln Loop",
-    author_email='info@lincolnloop.com',
+    description="AASHE's Campus Sustainability Hub",
+    author="AASHE, Lincoln Loop",
+    author_email='it@aashe.org, info@lincolnloop.com',
     url='',
     packages=find_packages(),
     package_data={'hub': ['static/*.*', 'templates/*.*']},
