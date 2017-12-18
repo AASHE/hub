af36c18283da6798d9996f942e3cc1e0249c9242
Brian Luft <brian@lincolnloop.com>
2017-12-18 13:59:04 -0800

    Don't apply ordering to search results; boost title and description; #281

--- hub/apps/browse/filter.py
+++ hub/apps/browse/filter.py
@@ -72,7 +72,14 @@ class SearchFilter(filters.CharFilter):
         query = Raw(query.lower())
         result_ids = (SearchQuerySet().filter(content__contains=query)
                                       .values_list('ct_pk', flat=True))
-        return qs.filter(pk__in=result_ids).distinct()
+        clauses = ' '.join(['WHEN id=%s THEN %s' % (pk, i) for i, pk in enumerate(result_ids)])
+        ordering = 'CASE %s END' % clauses
+        items = qs.filter(pk__in=result_ids).extra(
+            select={'ordering': ordering}, order_by=('ordering',))
+
+        # Workaround - we don't want the OrderingFilter to touch this later down the line
+        setattr(items, '__no_ordering__', True)
+        return items
 
 
 class TopicFilter(filters.ChoiceFilter):
@@ -347,6 +354,8 @@ class OrderingFilter(filters.ChoiceFilter):
         super(OrderingFilter, self).__init__(*args, **kwargs)
 
     def filter(self, qs, value):
+        if hasattr(qs, '__no_ordering__'):
+            return qs
         if not value:
             return qs.order_by('-published')
         return qs.order_by(value)
