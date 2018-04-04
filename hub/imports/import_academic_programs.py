7b979fa976a192c6dd21834caf3e1d734dd9ca8d
Tylor Dodge <tylor@tylorbook.local>
2018-04-04 13:05:22 -0400

    Set up import of academic programs

---
+++ hub/imports/import_academic_programs.py
@@ -0,0 +1,5 @@
+from utils import get_rows, get_base_kwargs, get_base_m2m, sanity_check
+from hub.apps.content.types.academic import AcademicProgram
+from hub.apps.metadata.models import ProgramType, SustainabilityTopic
+
+from datetime import datetime
