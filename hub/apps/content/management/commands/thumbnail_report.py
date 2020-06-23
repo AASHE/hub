fc5706c02f740be38f73348d19c0800e48fe21d4
Rogelio-AASHE <60620126+Rogelio-AASHE@users.noreply.github.com>
2020-06-23 16:31:56 -0600

    list broken thumbnail urls

--- hub/apps/content/management/commands/thumbnail_report.py
+++ hub/apps/content/management/commands/thumbnail_report.py
@@ -11,12 +11,16 @@ class Command(BaseCommand):
         total_images = Image.objects.count()
         broken_small_thumbnail_count = 0
         broken_med_thumbnail_count = 0
+        broken_images_small_urls = []
+        broken_images_med_urls = []
 
         for i in Image.objects.all():
             if i.small_thumbnail == "/static/img/100x100_blank.png":
                 broken_small_thumbnail_count += 1
+                print "broken small - " + i.ct.get_absolute_url()
             if i.med_thumbnail == "/static/img/300x300_blank.png":
                 broken_med_thumbnail_count += 1
+                print "broken med - " + i.ct.get_absolute_url()
 
         print "Total images: %d" % total_images
         print "Broken small thumbs: %d" % broken_small_thumbnail_count
