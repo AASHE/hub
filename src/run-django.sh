08674b9fc90539b6f1201be08a5ce322e7fb7de5
dspeck1 <dspeck@burwood.com>
2020-12-28 11:43:05 -0600

    initial commit of script to start django

---
+++ src/run-django.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+# Migrate Django after each build
+python manage.py migrate
+
+# Collect website static assets
+python manage.py collectstatic --noinput
+
+# Start Django
+gunicorn --bind :$PORT -w 2 --timeout 30 hub.wsgi:application
