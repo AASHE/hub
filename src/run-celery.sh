043d5aa2a7374e23cbc320e506cebbdfbf3f42f3
Jared Trouth <jtrouth@burwood.com>
2021-01-05 16:55:19 -0600

    Fix celery beat command

--- src/run-celery.sh
+++ src/run-celery.sh
@@ -9,4 +9,4 @@ python manage.py collectstatic --noinput
 celery -A stars worker --config stars/celery.py --loglevel=DEBUG &
 
 #Start Celery Beat
-celery beat --config stars/celery.py -A stars -s /var/www/stars/logs/beat.db --loglevel=DEBUG
+celery beat --config hub/celery.py -A hub -s /var/www/hub/logs/beat.db --loglevel=DEBUG
